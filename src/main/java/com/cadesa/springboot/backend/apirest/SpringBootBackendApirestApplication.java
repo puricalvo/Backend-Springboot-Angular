3ed40e7dbae19e66688548c8ea78d1ba173e0d5b
Puri Calvo <puri_adrian@hotmail.com>
2025-07-29 08:16:57 +0200

    Update SpringBootBackendApirestApplication.java

--- src/main/java/com/cadesa/springboot/backend/apirest/SpringBootBackendApirestApplication.java
+++ src/main/java/com/cadesa/springboot/backend/apirest/SpringBootBackendApirestApplication.java
@@ -19,7 +19,7 @@ public class SpringBootBackendApirestApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		String password = "12345";
+		String password = "*****";
 		
 		for (int i = 0; i < 4; i++) {
 			String passwordBcrypt = passwordEncoder.encode(password);
